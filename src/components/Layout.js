05fda3b993e057547e3668046d3b017146a75c49
Stephen Burakoff <svb@burakoff.com>
2019-06-28 16:41:28 -0400

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -16,7 +16,7 @@ export default class Body extends React.Component {
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                     <meta name="google" content="notranslate" />
                     <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i" rel="stylesheet"/>
-                    <link href="https://fonts.googleapis.com/css?family=Lato:400,900,900i&display=swap" rel="stylesheet">
+                    <link href="https://fonts.googleapis.com/css?family=Lato:400,900,900i&display=swap" rel="stylesheet"/>
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
                 </Helmet>
                 <div id="page" className={'site style-' + _.get(this.props, 'pageContext.site.siteMetadata.layout_style') + ' palette-' + _.get(this.props, 'pageContext.site.siteMetadata.palette')}>
